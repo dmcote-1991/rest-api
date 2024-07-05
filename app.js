77a6ac763cbfc8d0990cbfa94555166f1645d628
David Cote <dmc1991@icloud.com>
2024-07-05 12:44:25 -0400

    Adds Courses routes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const { Sequelize } = require('sequelize');
 const sequelize = require('./models').sequelize;
 const config = require('./config/config.json')
-const { User } = require('./models');
+const { User, Course } = require('./models');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -20,6 +20,9 @@ app.use(morgan('dev'));
 // setup JSON parsing for request bodies
 app.use(express.json());
 
+/*
+  * Home Route
+*/
 // setup a friendly greeting for the root route
 app.get('/', (req, res) => {
   res.json({
@@ -27,7 +30,10 @@ app.get('/', (req, res) => {
   });
 });
 
-// returns all properties and values for the currently authenticated User
+/*
+  * Users Routes
+*/
+// Returns all properties and values for the currently authenticated User
 app.get('/api/users', async (req, res) => {
   try {
     const authenticatedUserId = 1;
@@ -46,7 +52,7 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
-// creates a new user
+// Creates a new user
 app.post('/api/users', async (req, res) => {
   try{
     const newUser = await User.create(req.body);
@@ -54,6 +60,93 @@ app.post('/api/users', async (req, res) => {
   } catch (error) {
     console.error('Error creating user:', error);
     res.status(400).json({ message: 'Error creating user'});
+  }
+});
+
+/*
+  * Courses Routes
+*/
+// Returns all courses
+app.get('/api/courses', async (req, res) => {
+  try {
+    const courses = await Course.findAll({
+      attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded'],
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'firstName', 'lastName', 'emailAddress']
+      }
+    });
+    res.status(200).json(courses);
+  } catch (error) {
+    console.error('Error fetching courses', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Returns the corresponding course
+app.get('/api/courses/:id', async (req, res) => {
+  try {
+    const course = await Course.findByPk(req.params.id, {
+      attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded'],
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['id', 'firstName', 'lastName', 'emailAddress']
+      }
+    });
+
+    if (course) {
+      res.status(200).json(course);
+    } else {
+      res.status(404).json({ message: 'Course not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching course', error);
+    res.status(500).json({ message: 'Internal server error'});
+  }
+});
+
+// Creates a new course
+app.post('api/courses', async (req, res) => {
+  try {
+    const newCourse = await Course.create(req.body);
+    res.status(201).location(`/api/courses/${newCourse.id}`).end();
+  } catch (error) {
+    console.error('Error creating course', error);
+    res.status(400).json({ message: 'Error creating course' });
+  }
+});
+
+// Updates the corresponding course
+app.put('/api/courses/:id', async (req, res) => {
+  try {
+    const course = await Course.findByPk(req.params.id);
+    if (course) {
+      await course.update(req.body);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Course not found' });
+    }
+  } catch (error) {
+    console.error('Error updating course:', error);
+    res.status(400).json({ message: 'Error updating course' });
+  }
+});
+
+// Deletes the corresponding course
+app.delete('/api/courses/:id', async (req, res) => {
+  try {
+    const course = await Course.findByPk(req.params.id);
+    if (course) {
+      await course.destroy();
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Course not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting course:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
 
